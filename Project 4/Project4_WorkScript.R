f6497a70a4ef7a63e6b77db85f4e5c7e94bc9802
ilyakats <ikats@yahoo.com>
2018-07-03 23:43:39 -0400

    Project 4

--- Project 4/Project4_WorkScript.R
+++ Project 4/Project4_WorkScript.R
@@ -4,33 +4,45 @@ library(tidyr)
 
 # Import original file and select sample for project
 ratings <- read.csv("c:/temp/CUNY/data643/ratings_Beauty.csv")
+ratings <- read.csv("F:/CUNY/GitHub/ratings_Beauty.csv")
+
+# Explore
 head(ratings)
 class(ratings$UserId); class(ratings$ProductId); class(ratings$Rating); class(ratings$Timestamp)
 hist(ratings$Rating)
 
+# Convert to realRatingMatrix
 ratingsMatrix <- sparseMatrix(as.integer(ratings$UserId), as.integer(ratings$ProductId), x = ratings$Rating)
 colnames(ratingsMatrix) <- levels(ratings$ProductId)
 rownames(ratingsMatrix) <- levels(ratings$UserId)
-ratingsMatrix[1:50, 1:50]
-dim(ratingsMatrix)
-
 amazon <- as(ratingsMatrix, "realRatingMatrix")
+
+# Explore
 amazon 
 hist(rowCounts(amazon))
 table(rowCounts(amazon))
 hist(colCounts(amazon))
 table(colCounts(amazon))
 
+# Select Subset 1
 ( amazonShort <- amazon[rowCounts(amazon) > 10, colCounts(amazon) > 30] )
+
+# Select Subset 2
+amazonShort <- amazon[ , colCounts(amazon) > 30]
+amazonShort <- amazonShort[rowCounts(amazonShort) > 10, ]
+amazonShort
+
+# Check
 table(rowCounts(amazonShort))
 table(colCounts(amazonShort))
-( amazonShort <- amazonShort[ , colCounts(amazonShort) != 0] )
-amazonShort@data[1:100, 1:10]
-hist(rowCounts(amazonShort))
 
+# Remove empty items
+( amazonShort <- amazonShort[ , colCounts(amazonShort) != 0] )
+
+# Convert to data frame and save as CSV file
 df <- as.data.frame(as.matrix(amazonShort@data))
 df$UserId <- rownames(df)
 df <- df %>% gather(key = ProductId, value = Rating, -UserId) %>% filter(Rating != 0)
-# df[df == 0] <- NA
-# df[1:10, 1:10]
 write.csv(df, "c:/temp/CUNY/data643/ratings_Short.csv", row.names = FALSE)
+write.csv(df, "F:/CUNY/GitHub/ratings_Short.csv", row.names = FALSE)
+
