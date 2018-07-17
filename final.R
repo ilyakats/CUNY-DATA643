8f3cc9a3777ae8c740b13494d4e283a6259d5828
ilyakats <ikats@yahoo.com>
2018-07-17 14:54:55 -0400

    Final update

--- final.R
+++ final.R
@@ -1,3 +1,10 @@
+install.packages("sparklyr")
+install.packages("tictoc")
+
+
+library(tictoc)
+timing <- data.frame(Process=character(), Time=double())
+
 # ----------------------------- READING AWS S3 DATA INTO R -----------------------------
 
 install.packages("aws.s3")
@@ -16,64 +23,114 @@ obj <- get_object("ratings_Beauty_Short.csv", bucket = "data643summer18")
 beauty <- read.csv(text = rawToChar(obj))
 
 # Current csv file for video games
+tic()
 obj <- get_object("ratings_Video_Games.csv", bucket = "data643summer18")
-games <- read.csv(text = rawToChar(obj))
+games <- read.csv(text = rawToChar(obj), header = FALSE)
+colnames(games) <- c("UserID", "ProductID", "Rating", "TimeStamp")
+t <- toc(quiet = TRUE)
+timing <- rbind(timing, data.frame(Process = "Load Data from S3 into Local Data Frame", Time = round(t$toc - t$tic, 2)))
 
+rm(obj)
 
-# ----------------------------- READING AWS S3 DATA INTO SPARK -----------------------------
+class(games)
+dim(games)
+head(games)
+length(unique(games$UserID))
+length(unique(games$ProductID))
+
+# ----------------------------- MOVING DATA INTO SPARK -----------------------------
 
 library(sparklyr)
 library(dplyr)
 
+# LOCAL CONNECTION
+tic()
 config <- spark_config()
-config$`spark.hadoop.fs.s3a.impl` <- "org.apache.hadoop.fs.s3a.S3AFileSystem"
-config$`fs.s3a.access.key` <- "AKIAJUGWG3SNEZGRRDTA"
-config$`fs.s3a.secret.key` <- "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"
-
+config$spark.executor.memory <- "2GB"
+config$spark.memory.fraction <- 0.9
+#config$`spark.hadoop.fs.s3a.impl` <- "org.apache.hadoop.fs.s3a.S3AFileSystem"
+#config$`fs.s3a.access.key` <- "AKIAJUGWG3SNEZGRRDTA"
+#config$`fs.s3a.secret.key` <- "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"
 sc <- spark_connect(master = "local", config = config)
+t <- toc(quiet = TRUE)
+timing <- rbind(timing, data.frame(Process = "Establish Spark Connection", Time = round(t$toc - t$tic, 2)))
 
-beauty_tbl <- spark_read_csv(sc, name = "beauty_ratings", path = "s3a://data643summer18/ratings_Beauty_Short.csv")
+# SERVER CONNECTION
+tic()
+config <- spark_config()
+#config$spark.executor.cores <- 2
+#config$spark.dynamicAllocation.enabled <- "false"
+config$spark.executor.memory <- "2GB"
+config$spark.memory.fraction <- 0.9
+#config$spark.driver.extraJavaOptions <- "-Dcom.amazonaws.services.s3.enableV4"
+sc <- spark_connect(master="spark://ip-172-31-41-22.ec2.internal:7077", 
+                    version = "2.1.0",
+                    config = config,
+                    spark_home = "/home/ubuntu/spark-2.1.0-bin-hadoop2.7/")
+t <- toc(quiet = TRUE)
+timing <- rbind(timing, data.frame(Process = "Establish Spark Connection", Time = round(t$toc - t$tic, 2)))
 
-spark_disconnect(sc)
-
-
-#Get spark context 
+# Reading data directly from S3 into Spark is not working
 ctx <- spark_context(sc)
-#Use below to set the java spark context 
 jsc <- invoke_static(sc, "org.apache.spark.api.java.JavaSparkContext", "fromSparkContext", ctx)
-
-#set the s3 configs: 
 hconf <- jsc %>% invoke("hadoopConfiguration")
-hconf %>% invoke("set","spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 hconf %>% invoke("set","fs.s3a.access.key", "AKIAJUGWG3SNEZGRRDTA")
 hconf %>% invoke("set","fs.s3a.secret.key", "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")
+games <- spark_read_csv(sc, name = "games_df", 
+                        path = "s3a://data643summer18/ratings_Video_Games.csv")
 
-usercsv_tbl <- spark_read_csv(sc, name = "beautytbl",path = "s3a://data643summer18/ratings_Beauty_Short.csv")
+beauty <- beauty[1:10,]
+beauty_sp <- sdf_copy_to(sc, beauty, "beauty_df", overwrite = TRUE)
 
-library(tidyverse)
-library(sparklyr)
+# Copy data to Spark
+tic()
+games_sp <- sdf_copy_to(sc, games[1:100,], "games_df", overwrite = TRUE)
+t <- toc(quiet = TRUE)
+timing <- rbind(timing, data.frame(Process = "Copy Data Frame to Spark", Time = round(t$toc - t$tic, 2)))
+
+head(games_sp)
+
+# Convert IDs to numeric for use in ALS
+games_sp <- games_sp %>%
+  ft_string_indexer(input_col = "UserID", output_col = "UserIDn") %>%
+  ft_string_indexer(input_col = "ProductID", output_col = "ProductIDn")
+
+# Split for trainig and testing
+games_partition <- sdf_partition(games_sp, training = 0.8, testing = 0.2)
+sdf_register(games_partition, c("games_train","games_test"))
+
+games_tidy_train <- tbl(sc,"games_train") %>%
+  select(UserIDn, ProductIDn, Rating)
+
+# Build model
+tic()
+sparkALS <- ml_als(games_tidy_train, max_iter = 5, nonnegative = TRUE, 
+                   rating_col = "Rating", user_col = "UserIDn", item_col = "ProductIDn")
+t <- toc(quiet = TRUE)
+timing <- rbind(timing, data.frame(Process = "Building ALS Model", Time = round(t$toc - t$tic, 2)))
+
+# Run prediction
+games_test <- tbl(sc, "games_test")
+
+pred_iris <- sdf_predict(
+  model_iris, test_iris) %>%
+  collect 
 
 
-
-config$`sparklyr.shell.driver-java-options` <- paste0("-Djava.io.tmpdir=", 
-                                                      spark_home_dir())
-config$`sparklyr.shell.driver-memory` <- "4G"
-config$`sparklyr.shell.executor-memory` <- "4G"
-config$`spark.yarn.executor.memoryOverhead` <- "512"
-sc <- spark_connect(master = "local", config = config, version = '2.3.0')
-
-rvw5 <- spark_read_json(sc, name = "five_reviews",
-                        header = TRUE, 
-                        path = "Video_Games_5.json")
+tic()
+sparkPred <- sparkALS$.jobj %>%
+  invoke("transform", spark_dataframe(spark_test)) %>%
+  collect()
+t <- toc(quiet = TRUE)
 
 
+sparkPred <- sparkPred[!is.na(sparkPred$prediction), ] # Remove NaN due to data set splitting
 
-Sys.getenv("SPARK_HOME")
-Sys.setenv(SPARK_HOME="C:/Users/ikats/AppData/Local/spark/spark-2.1.0-bin-hadoop2.7/bin")
-sc <- spark_connect(master = "spark://54.173.106.94", version="2.1.0")
-connection_is_open(sc)
+# Calculate error
+mseSpark <- mean((sparkPred$Rating - sparkPred$prediction)^2)
+rmseSpark <- sqrt(mseSpark)
+maeSpark <- mean(abs(sparkPred$Rating - sparkPred$prediction))
+
+
+# Disconnect from Spark
 spark_disconnect(sc)
-
-
-
-livy_install()
